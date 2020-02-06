4f969e027f868333a37173e3f1cdf419c26bcffc
haidichen <chenhaidi43@163.com>
2020-02-05 18:03:00 -0600

    separate Pi3 and Pi4 setup

---
+++ RaspberryPi_Server_Development/Pi4/HoloPi/HoloPi_Server.py
@@ -0,0 +1,126 @@
+# usage:
+# python3 HoloPi_Server.py -t [IP Address of the other Pi]
+from flask import Flask, jsonify 
+from flask_restful import Resource, Api, reqparse
+import werkzeug, os
+from werkzeug.utils import secure_filename
+import base64
+import requests
+from ncsobjdetection import detectFromImage
+import datetime
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-t', '--targetIp', required=True, help='the IP address of another pi')
+args = vars(ap.parse_args())
+
+if os.path.isdir('Images') == False:
+    os.mkdir('Images')
+    os.mkdir('Images/Uploaded')
+    os.mkdir('Images/DetectionResults')
+
+app = Flask(__name__)
+api = Api(app)
+
+UPLOAD_FOLDER = 'Images/Uploaded/'
+
+parser = reqparse.RequestParser()
+parser.add_argument('file',
+        type=werkzeug.datastructures.FileStorage,
+        location='files')
+parser.add_argument('sharing', type=dict, action='append', location='json')
+parser.add_argument('sourceIp', type=str, location='json')
+
+sharingString = [] 
+sourceIp = ''
+
+class Connecting(Resource):
+    def get(self):
+        return 1 
+
+class Detecting(Resource):
+    def post(self):
+        data = parser.parse_args()
+        if data['file'] == None:
+            return "no file"
+        photo = data['file']
+
+        if photo:
+            filename = secure_filename(photo.filename) 
+            d = datetime.datetime.now()
+            directory = UPLOAD_FOLDER + d.date().isoformat()
+            if os.path.isdir(directory) == False:
+                os.mkdir(directory)
+
+            filePath = os.path.join(directory, filename)
+
+            photo.save(filePath)
+
+            with open(filePath, mode="rb") as fi:
+                imageString = base64.b64encode(fi.read())
+
+            detectionResults = [{'ItemName': filename, 'ItemImage': imageString.decode(),
+                'ItemDescription':'This is the original picture you uploaded.'}]
+
+            detectionResults += detectFromImage(filePath)
+            return jsonify(detectionResults)
+
+class Sharing(Resource):
+    def post(self):
+        data = parser.parse_args()
+        if data['sharing'] == None:
+            return  
+
+        #send the sharing string to other RP
+        DestIP = args['targetIp']
+        try:
+            resp = requests.post("http://" + DestIP + ":5000/store", json={"sharing": data['sharing'], 'sourceIp':request.remote_addr})
+        except :
+            return 2
+        else:
+            if resp.json() == 1:
+                return 1
+            else:
+                return 0
+
+class Receiving(Resource):
+    def get(self):
+        global sharingString
+        if len(sharingString) > 0:
+            copy = [{'sourceIp':sourceIp}]
+            copy += sharingString
+            sharingString = []
+            return jsonify(copy)
+        else:
+            return 0
+
+class Storing(Resource):
+    def post(self):
+        global sharingString
+        global sourceIp
+
+        data = parser.parse_args()
+        if data['sharing'] == None:
+            return  
+
+        # store the sharing data to the local variable
+        sharingString += data['sharing']
+        sourceIp = data['sourceIp']
+        return 1
+
+class Notifying(Resource):
+    def get(self):
+        if len(sharingString) > 0:
+            return 1
+        else:
+            return 0
+       
+api.add_resource(Connecting, '/')
+api.add_resource(Detecting, '/detect')
+api.add_resource(Sharing, '/share')
+api.add_resource(Receiving, '/receive')
+api.add_resource(Storing, '/store')
+api.add_resource(Notifying, '/notify')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ RaspberryPi_Server_Development/Pi4/setup.sh
@@ -0,0 +1,53 @@
+#1. install the OS Raspbian Full
+
+#2. update and upgrade the system
+sudo apt update && sudo apt upgrade -y
+
+#3. enable ssh
+sudo apt install -y openssh-server
+
+sudo sytemctl restart ssh
+
+#4. install vim, git, libatlas-base-dev
+sudo apt install -y vim git libatlas-base-dev
+
+#5. copy the HoloPi project folder to the Desktop
+cp -r HoloPi ~/Desktop/
+
+#6. install openVINO toolkit
+sudo mkdir -p /opt/intel/openvino
+sudo tar -xf l_openvino_toolkit_runtime_raspbian_p_2019.3.334.tgz --strip 1 -C /opt/intel/openvino/
+
+#7. install cmake
+sudo apt install -y cmake
+
+#8. setup openvino variables
+echo "source /opt/intel/openvino/bin/setupvars.sh" >> ~/.bashrc
+
+#9. add current user to users group 
+#sudo usermod -a -G users "$(whoami)"
+
+#10. install USB rules
+bash /opt/intel/openvino/install_dependencies/install_NCS_udev_rules.sh
+
+#11. build and run sample
+cd ~/Desktop/
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-march=armv7-a" /opt/intel/openvino/deployment_tools/inference_engine/samples
+make -j2 object_detection_sample_ssd
+
+#12. download pre-trained face detection model
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.bin
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R1/models_bin/face-detection-adas-0001/FP16/face-detection-adas-0001.xml
+cp ~/Desktop/holo-pi/test-image.jpg .
+./armv7l/Release/object_detection_sample_ssd -m face-detection-adas-0001.xml -d MYRIAD -i test-image.jpg
+
+#13. check openCV installation
+cp ~/Desktop/holo-pi/Raspberry_Server_Development/Pi4/openvino_fd_myriad.py .
+python3 openvino_fd_myriad.py
+
+#14. install python modules
+cd ~/Desktop
+python3 -m venv ncsod
+. ncsod/bin/active
+pip install -r ~/Desktop/holo-pi/RaspberryPi_Server_Development/Pi4/requirements.txt
